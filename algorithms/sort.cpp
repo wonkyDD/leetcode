f94e4890d503ab8d41805edc798661a584d55f3a
wonkydd <dev.csh.01@gmail.com>
2022-10-20 21:29:13 +0900

    :sparkles: Add: init various algorithms and data-structures

---
+++ algorithms/sort.cpp
@@ -0,0 +1,26 @@
+// https://atomic0x90.github.io/c++/2020/02/22/c++-sort.html
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+using namespace std;
+
+void sort() {
+
+}
+
+int main() {
+
+    int arr[10] = {5, 90, 87, 36, 19, 28, 37, 10, 2, 9};
+
+    // left, right 전략
+    // 오름차순, 내림차순 구현
+    // compare 구현
+    // cout<<*arr<<endl;
+    // unordered_map<string, int> um;
+
+    unordered_map<int, int> im
+
+
+    return 0;
+}
