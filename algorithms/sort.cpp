0bce1171bd59414a71c45f5fc13a3ce8ae7d3d5d
wonkydd <dev.csh.01@gmail.com>
2022-10-24 07:56:48 +0900

    :bulb: add comment for spec

--- algorithms/sort.cpp
+++ algorithms/sort.cpp
@@ -1,26 +1,13 @@
 // https://atomic0x90.github.io/c++/2020/02/22/c++-sort.html
 #include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <string>
 using namespace std;
 
 void sort() {
-
+    // std::sort(g_testEntries, g_testEntries + g_testCount, CompareTests);
+    // 이런 식으로 3번째 인자도 구현할 것 (오름차순, 내림차순 모두!!)
 }
 
 int main() {
-
     int arr[10] = {5, 90, 87, 36, 19, 28, 37, 10, 2, 9};
-
-    // left, right 전략
-    // 오름차순, 내림차순 구현
-    // compare 구현
-    // cout<<*arr<<endl;
-    // unordered_map<string, int> um;
-
-    unordered_map<int, int> im
-
-
     return 0;
 }
