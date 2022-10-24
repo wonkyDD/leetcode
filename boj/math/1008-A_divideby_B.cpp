4eea2a878f07b2ae04a315f252ecc81b74976a6a
wonkydd <dev.csh.01@gmail.com>
2022-10-24 09:15:45 +0900

    :sparkles: solve `1008-A_divideby_B`

---
+++ boj/math/1008-A_divideby_B.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problemset?sort=ac_desc&algo=124
+// https://st-lab.tistory.com/212
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main() {
+    double i,j;
+    cin>>i>>j;
+    printf("%.13lf", i / j);
+    return 0;
+}
