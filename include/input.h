598ac90b7251a9c0f844c5d3e170e63de911976d
wonkydd <dev.csh.01@gmail.com>
2022-12-17 03:12:58 +0900

    :art: feat: input class for boj, codeforces

---
+++ include/input.h
@@ -0,0 +1,30 @@
+/**
+ * @todo
+ * make it easy to test with various kinds of inputs
+*/
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <iostream>
+#include <vector>
+// using namespace std;
+
+class Input
+{
+public:
+    Input() = default;
+};
+
+class InputBOJ : public Input
+{
+public:
+    InputBOJ() = default;
+};
+
+class InputCodeforces : public Input
+{
+public:
+    InputCodeforces() = default;
+};
+
+#endif // INPUT_H
--- include/wonkydd.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void sayHello() {
-    std::cout<<"sayHello"<<std::endl;
-}
