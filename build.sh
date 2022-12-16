7f92f64e9f8ecb9b3a80c18a75cebfeb10001b62
wonkydd <dev.csh.01@gmail.com>
2022-12-16 16:54:52 +0900

    :fire::hammer: fix: replacd shell with python script

--- build.sh
+++
@@ -1,4 +0,0 @@
-# CSAPP Lab 프로젝트 코드들 (Makefile -> cmake)
-# Game들 cmake로 포팅 (macOS, window 빌드 모두 지원)
-# algorithms 빌드, 테스트, 벤치마킹
-# .vscode의 tasks.json과 엮어서 빌드 -> exe 또는 bin 파일실행
---
+++ script.py
@@ -0,0 +1,13 @@
+"""cross-platform을 위해 shell->py로 변경"""
+
+# @TODO build.sh
+# CSAPP Lab 프로젝트 코드들 (Makefile -> cmake)
+# Game들 cmake로 포팅 (macOS, window 빌드 모두 지원)
+# algorithms 빌드, 테스트, 벤치마킹
+# .vscode의 tasks.json과 엮어서 빌드 -> exe 또는 bin 파일실행
+
+# @TODO local.sh
+# renew `platform/` from local `.vscode` json files
+
+if __name__ == "__main__":
+    print("Hello")
