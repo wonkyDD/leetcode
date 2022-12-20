bfb4ece5c7624a004c77d16933ff5c1839f8a081
wonkydd <dev.csh.01@gmail.com>
2022-12-21 07:06:24 +0900

    :bulb: update

--- script.py
+++ script.py
@@ -1,15 +1,9 @@
-"""cross-platform을 위해 shell->py로 변경"""
+"""For cross-platform, use python"""
 
 # sys.platform == darwin || sys.platform == win32
 
-# @TODO build.sh
-# CSAPP Lab 프로젝트 코드들 (Makefile -> cmake)
-# Game들 cmake로 포팅 (macOS, window 빌드 모두 지원)
-# algorithms 빌드, 테스트, 벤치마킹
-# .vscode의 tasks.json과 엮어서 빌드 -> exe 또는 bin 파일실행
-
-# @TODO local.sh
-# renew `platform/` from local `.vscode` json files
+# @TODO
+# - renew `platform/` from local `.vscode` json files
 
 if __name__ == "__main__":
     print("Hello")
