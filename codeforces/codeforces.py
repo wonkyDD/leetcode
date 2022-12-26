c668f9be249c1613148a307762230970f5852a84
wonkydd <dev.csh.01@gmail.com>
2022-12-26 10:11:05 +0900

    :hammer: automate file genearation for each of rounds

---
+++ codeforces/codeforces.py
@@ -0,0 +1,7 @@
+# @NOTE : codeforces provide API, so use it if needed
+# @TODO
+# 1. get problem link
+# 2. get solution link (/blog/?)
+# 3. mkdir `{round_number}/`
+# 4. make cpp files named as `{round_number}{problem_alpahbet}-{problem_title}.cpp`
+# 5. add problem link, solution link, and `#include <bits/stdc++.h>` on top of file
